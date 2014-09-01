ed65002396d4307ffef747b582190c7176269686
yangyang <1428491590@qq.com>
2014-09-01 10:38:04 +0800

    初始化yii,加入bootstrap,使用column3

---
+++ assets/bootstrap3/bootstrap3.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Bootstrap 实例</title>
+   <meta charset=utf-8>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+   <![endif]-->
+   <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <script src="./bootstrap/js/jquery.min.js"></script>
+   <script src="./bootstrap/js/bootstrap.min.js"></script>
+   <script src="./bootstrap/js/holder.min.js"></script>
+   <script src="./bootstrap/js/application.js"></script>
+   <style>
+      .col-md-12{
+          background: #ccc;
+      }
+   </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>bootstrap col-md-12</h3>
+                <p> 据英国《每日邮报》报道，美国肯塔基州一对变性人夫妇目前已经生育两个孩子。因两人还保留着原始的性器官，因此，作为丈夫的27岁的尼克实际上扮演了母亲的角色，怀孕生子。</p>
+                <p> 32岁的妻子比安卡扮演了父亲的角色，贡献了精子。两人都是变性人，尼克出生时叫妮科尔，是个女孩，但尼克说自己是个男孩，只是生在了错误的身体里。而比安卡原本是个男孩，在经过隆胸、脱毛等手术后，过上了女性的生活。</p>
+            </div>
+            <div class="col-md-3">
+                <img src="holder.js/200x200" alt="">
+            </div>
+            <div class="col-md-3">
+                <img src="holder.js/200x200" alt="">
+            </div>
+            <div class="col-md-3">
+                <img src="holder.js/200x200" alt="">
+            </div>
+            <div class="col-md-3">
+                <img src="holder.js/200x200" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <p> 据英国《每日邮报》报道，美国肯塔基州一对变性人夫妇目前已经生育两个孩子。因两人还保留着原始的性器官，因此，作为丈夫的27岁的尼克实际上扮演了母亲的角色，怀孕生子。</p>
+        </div>
+        <div class="row">
+            <p> 据英国《每日邮报》报道，美国肯塔基州一对变性人夫妇目前已经生育两个孩子。因两人还保留着原始的性器官，因此，作为丈夫的27岁的尼克实际上扮演了母亲的角色，怀孕生子。</p>
+        </div>
+        <div class="row">
+             <div class="col-md-4">
+                <img src="holder.js/200x200" alt="">
+            </div>
+            <div class="col-md-4">
+                <img src="holder.js/200x200" alt="">
+            </div>
+            <div class="col-md-4">
+                <img src="holder.js/200x200" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ protected/views/layouts/column3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+         <head>
+                  <title>Bootstrap 实例</title>
+                  <meta charset=utf-8>
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <!--[if lt IE 9]>
+                     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+                  <![endif]-->
+                  <link href="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+                  <link href="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/css/home.css" rel="stylesheet">
+                  <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/jquery.min.js"></script>
+                  <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/bootstrap.min.js"></script>
+                  <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/holder.min.js"></script>
+                  <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/application.js"></script>
+                   <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/highlight.min.js"></script>
+                   <script src="<?php echo  Yii::app()->baseUrl; ?>/assets/bootstrap3/bootstrap/js/home.js"></script>
+                  <style>
+                  
+                  </style>
+         </head>
+         <body>
+
+         </body>
+</html>
